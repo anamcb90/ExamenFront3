d77e63ec42ff4a317b0e5397a2fbcc17d15a89dc
anamcb90 <amcorreab0619@gmail.com>
2023-10-30 18:37:03 -0500

    clase7

---
+++ examen_front3-clase11/src/App.jsx
@@ -0,0 +1,21 @@
+import Card from "./Components/Card"
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className = 'app'>
+       <h1>Carga de estudiantes</h1>
+       <form></form>
+       
+       <Card  />
+  
+        </div>
+ 
+    </>
+  )
+}
+
+export default App
